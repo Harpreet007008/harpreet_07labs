453d0f808ddc32d30678687882690548a4e8c7c1
Harpreet Singh <sing1479@algonquinlive.com>
2021-06-11 15:07:34 -0400

    week4

--- app/src/main/java/com/example/harpreetsandroidlab/MainActivity.java
+++ app/src/main/java/com/example/harpreetsandroidlab/MainActivity.java
@@ -1,33 +1,51 @@
 package com.example.harpreetsandroidlab;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("MainActivity", "In onCreate() - Loading Widgets" );
 
         Button loginBtn = findViewById(R.id.nextPageButton);
-        Log.w("MainActivity", "In onCreate() - Loading Widgets" );
+        EditText emailEditText =findViewById(R.id.inputEditText);
 
         loginBtn.setOnClickListener( clk -> {
-            EditText et = findViewById(R.id.inputEditText);
-            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
 
-            nextPage.putExtra("SomeInfo", et.getText().toString());
+            //This Intent say that you want to transition from MainActivity to SecondActivity
+
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            nextPage.putExtra( "EmailAddress", emailEditText.getText().toString() );
+
             startActivity( nextPage );
         });
     }
+
+    @Override  //screen is now visible
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "In onCreate() - The application is now visible on screen" );
+    }
+
+    @Override  //screen is now listening for input
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "In onCreate() - The application is now responding to user input" );
+    }
+
 
 }
--- app/src/main/java/com/example/harpreetsandroidlab/Secondactivity.java
+++ app/src/main/java/com/example/harpreetsandroidlab/Secondactivity.java
@@ -1,21 +1,61 @@
 package com.example.harpreetsandroidlab;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 class SecondActivity extends AppCompatActivity {
+
+    private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        EditText phonenumber = findViewById(R.id.editTextPhone);
+        Editable phonenumberText = phonenumber.getText();
+        TextView Topofthescreen = findViewById(R.id.textView);
+
         Intent fromPrevious = getIntent();
-        String text = fromPrevious.getStringExtra("SomeInfo");
+        String text = fromPrevious.getStringExtra("EmailAddress");
+        Topofthescreen.setText("Welcome Back:" + text);
 
+        Button btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(clk ->{
+            Intent next = new Intent(Intent.ACTION_DIAL);
+            next.setData(Uri.parse("tel:" + phonenumberText));
+            startActivity(next);
+        });
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(clk -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult( cameraIntent, 3456);
+        });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 3456)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Bitmap thumbnail = data.getParcelableExtra("data");
+                profileImage.setImageBitmap(thumbnail);
+            }
+        }
 
     }
 }
