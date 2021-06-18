b46b22b6cf3b9cef54827a9056af39505c423c9b
Harpreet Singh <sing1479@algonquinlive.com>
2021-06-18 14:49:09 -0400

    week 5 recycle

--- app/src/main/java/com/example/harpreetsandroidlab/MainActivity.java
+++ app/src/main/java/com/example/harpreetsandroidlab/MainActivity.java
@@ -1,20 +1,18 @@
 package com.example.harpreetsandroidlab;
 
-import androidx.annotation.Nullable;
-        import androidx.appcompat.app.AppCompatActivity;
 
-        import android.content.Intent;
-        import android.graphics.Bitmap;
-        import android.os.Bundle;
-import android.provider.MediaStore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.util.Log;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    final private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,46 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginBtn = findViewById(R.id.nextPageButton);
-        Log.v(TAG , "In onCreate() - Loading Widgets" );
-
+        Log.w("MainActivity", "In onCreate() - Loading Widgets" );
 
         loginBtn.setOnClickListener( clk -> {
             EditText et = findViewById(R.id.inputEditText);
-
-            //This Intent say that you want to transition from MainActivity to SecondActivity
-
             Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-            nextPage.putExtra("some Info", et.getText().toString());
-            nextPage.putExtra("MyFloat", 3.14f);
-            nextPage.putExtra("IsTrue", false);
-            Intent call =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-            startActivity( nextPage   );
+            nextPage.putExtra("SomeInfo", et.getText().toString());
+            startActivity( nextPage );
         });
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==123){
-            Log.w(TAG, "coming back from next page");
-
-        }
-    }
-
-
-
-    @Override  //screen is now visible
-    protected void onStart() {
-        super.onStart();
-        Log.v(TAG, "In onCreate() - The application is now visible on screen" );
-    }
-
-    @Override  //screen is now listening for input
-    protected void onResume() {
-        super.onResume();
-        Log.v(TAG, "In onCreate() - The application is now responding to user input" );
-    }
-
 
 }
---
+++ app/src/main/java/com/example/harpreetsandroidlab/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.harpreetsandroidlab;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent fromPrevious = getIntent();
+        String text = fromPrevious.getStringExtra("SomeInfo");
+
+
+
+    }
+}
