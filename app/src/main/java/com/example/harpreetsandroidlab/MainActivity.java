81f11a6ba5cb68e082c311c3156e42a6ef7380a5
Harpreet Singh <sing1479@algonquinlive.com>
2021-06-25 22:36:31 -0400

    week6

--- app/src/main/java/com/example/harpreetsandroidlab/MainActivity.java
+++ app/src/main/java/com/example/harpreetsandroidlab/MainActivity.java
@@ -1,33 +1,50 @@
 package com.example.harpreetsandroidlab;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText passwordText;
+
+
+    private TextView textView;
+
+
+    private Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginBtn = findViewById(R.id.nextPageButton);
-        Log.w("MainActivity", "In onCreate() - Loading Widgets" );
+        passwordText = findViewById(R.id.pw);
+        textView = findViewById(R.id.textView);
+        login = findViewById(R.id.loginButton);
 
-        loginBtn.setOnClickListener( clk -> {
-            EditText et = findViewById(R.id.inputEditText);
-            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+        login.setOnClickListener( clk -> {
+            String password = passwordText.getText().toString();
 
-            nextPage.putExtra("SomeInfo", et.getText().toString());
-            startActivity( nextPage );
+            if(checkPassword( password ))//check if password is complex enough
+            {
+                textView.setText("Your password has ABC");
+            }
+            else textView.setText("No ABC string was found");
+
+
         });
     }
 
+
+    private boolean checkPassword(String password) {
+        return password.contains("ABC");
+    }
 }
